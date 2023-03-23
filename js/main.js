b343a81d3cdbca2e17ffa9679c8d913ceff78429
filipskrabak <xskrabakf@stuba.sk>
2023-03-23 11:02:29 +0100

    add clickable searchbar

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 $(document).ready(function(){
-    $('#search').on("click",(function(e){
-    $(".form-group").addClass("sb-search-open");
-      e.stopPropagation()
-    }));
-     $(document).on("click", function(e) {
-      if ($(e.target).is("#search") === false && $(".form-control").val().length == 0) {
-        $(".form-group").removeClass("sb-search-open");
+    $('#searchBarBtn').on("click",(function(e){
+      if($('#searchBar').hasClass("d-none")) {
+        $('#searchBar').removeClass("d-none");
+        $('#floatingSearch').focus();
       }
-    });
-      $(".form-control-submit").click(function(e){
-        $(".form-control").each(function(){
-          if($(".form-control").val().length == 0){
-            e.preventDefault();
-            $(this).css('border', '2px solid red');
-          }
-      })
-    })
+      else {
+        $('#searchBar').addClass("d-none");
+      }
+
+    }));
 
     $('.dropdown').on("mouseenter", function() {
       if($(window).width() > 576) {
