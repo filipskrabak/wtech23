9d82a77924f0bbac0d13658467cda24bf7492158
filipskrabak <xskrabakf@stuba.sk>
2023-04-05 19:43:33 +0000

    fix github deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,27 +5,27 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(sudo php artisan down) || true
+(php artisan down) || true
 
 # Pull the latest version of the app
 git pull origin
 
 # Install composer dependencies
-sudo composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache
-sudo php artisan clear-compiled
+php artisan clear-compiled
 
 # Recreate cache
-sudo php artisan optimize
+php artisan optimize
 
 # Compile npm assets
-npm run prod
+#npm run prod
 
 # Run database migrations
-sudo php artisan migrate --force
+php artisan migrate --force
 
 # Exit maintenance mode
-sudo php artisan up
+php artisan up
 
 echo "Deployment finished!"
