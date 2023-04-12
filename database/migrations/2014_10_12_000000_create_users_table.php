1c2dabc27f534a828db9a47af511af4ad3a7482a
filipskrabak <xskrabakf@stuba.sk>
2023-04-12 02:06:20 +0200

    add users migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            // TODO: add address
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',32);
+            $table->string('surname',32);
+            $table->string('phone',15);
+            $table->string('email', 96)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
