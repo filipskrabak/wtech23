741ead8384a858ab625ea877d49d6f5ab6a290fa
filipskrabak <xskrabakf@stuba.sk>
2023-04-12 02:17:17 +0200

    add product M/C

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // Get and show all products
+    public function index() {
+
+    }
+
+    // Show single product
+    public function show() {
+
+    }
+}
