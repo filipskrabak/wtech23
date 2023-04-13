9cf59636abe22048106cf1fa2ff0d60fabed5164
patrikprizbul <patrikprizbul@gmail.com>
2023-04-13 23:12:54 +0200

    add OrderProducts, small changes

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Variant extends Model
+class OrderProducts extends Model
 {
     use HasFactory;
-
-    public function orders(){
-        return $this->hasMany(OrderProducts::class);
-    }
 }
---
+++ database/migrations/2023_04_13_210349_create_order_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Variant;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Order::class)->constrained();
+            $table->foreignIdFor(Variant::class)->constrained();
+            $table->integer('pcs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_products');
+    }
+};
