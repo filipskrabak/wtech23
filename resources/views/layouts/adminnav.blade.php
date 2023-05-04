7486414138050e20362a795df58c4ec2ccbb3d3c
filipskrabak <xskrabakf@stuba.sk>
2023-05-05 00:45:34 +0200

    add active class

--- resources/views/layouts/adminnav.blade.php
+++ resources/views/layouts/adminnav.blade.php
@@ -3,16 +3,16 @@
         <div class="collapse navbar-collapse collapsibleNav" id="navbarNav" >
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <a class="nav-link" href="/dashboard/products">Products</a>
+                <a class="nav-link {{ (request()->is('dashboard/products')) ? 'active' : '' }}" href="/dashboard/products">Products</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" href="/dashboard/users">Users</a>
+                <a class="nav-link {{ (request()->is('dashboard/users')) ? 'active' : '' }}" href="/dashboard/users">Users</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link"  href="/dashboard/orders">Orders</a>
+                <a class="nav-link {{ (request()->is('dashboard/orders')) ? 'active' : '' }}"  href="/dashboard/orders">Orders</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link"  href="/dashboard/attribute-values">Attribute Values</a>
+                <a class="nav-link {{ (request()->is('dashboard/attribute-values')) ? 'active' : '' }}"  href="/dashboard/attribute-values">Attribute Values</a>
                 </li>
             </ul>
         </div>
