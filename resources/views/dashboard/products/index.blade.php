4102a54ef86fe1070799dd32fcd746901c73f25f
patrikprizbul <patrikprizbul@gmail.com>
2023-05-04 20:34:53 +0200

    change buttons style

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -12,7 +12,7 @@
             <h1 class="text-left">Dashboard - products</h1>
         </div>
         <div class="col-md-3">
-            <a href="/dashboard/products/create" class="btn btn-primary w-100">Add product</a>
+            <a href="/dashboard/products/create" class="btn btn-success w-100"><i class="fa-solid fa-plus me-1"></i>Add product</a>
         </div>
     </div>
     <table class="table mt-3 table-striped">
@@ -31,12 +31,12 @@
                 <td>{{$product->price}}</td>
                 <td>
                     <div class="container d-flex justify-content-end">
-                        <a href="/product/{{$product->slug}}" class="btn btn-sm btn-primary">View</a>
-                        <a href="/dashboard/products/{{$product->slug}}/edit" class="btn btn-sm btn-primary mx-2">Edit</a>
+                        <a href="/product/{{$product->slug}}" class="btn btn-sm btn-primary"><i class="fa-solid fa-shirt me-1"></i>View</a>
+                        <a href="/dashboard/products/{{$product->slug}}/edit" class="btn btn-sm btn-warning mx-2"><i class="fa-solid fa-pen-to-square me-1"></i>Edit</a>
                         <form method="POST" action="/dashboard/products/{{$product->slug}}">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-sm btn-primary">Delete</button>
+                            <button class="btn btn-sm btn-danger"><i class="fa-solid fa-xmark me-1"></i>Delete</button>
                         </form>
                     </div>
                 </td>
