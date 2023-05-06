13a76658211196afd9672b59807e9b20b1fd7d5f
Samuel Krempaský <36561335+NIKTONIKTO717@users.noreply.github.com>
2023-05-06 20:20:24 +0200

    Adde shipping page

---
+++ resources/views/shipping.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('title', 'Shipping Methods')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center align-items-center g-2">
+        <div class="col">
+            <h1 class="mt-5 mb-4 text-center">Shipping Methods</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center align-items-center g-2">
+        <div class="col">
+        These are three popular shipping methods that you can use to deliver your packages:
+
+        DHL: DHL is one of the world's leading logistics and shipping companies, offering fast and reliable shipping services to businesses and individuals worldwide. With DHL, you can ship your packages for as low as 2€. DHL provides a range of shipping options, including express and standard shipping, to ensure that your packages are delivered on time.
+
+        Slovneská pošta: Slovneská pošta is the national postal service provider of Slovakia. It offers a wide range of shipping services, including domestic and international shipping, parcel delivery, and express shipping. With Slovneská pošta, you can ship your packages for as low as 2€. Slovneská pošta also provides tracking and insurance services to ensure that your packages are safe and secure during transit.
+
+        GLS: GLS is a leading European logistics and shipping company, providing reliable and cost-effective shipping services across Europe and beyond. With GLS, you can ship your packages for as low as 2€. GLS offers a range of shipping options, including express and standard shipping, as well as additional services such as delivery confirmation and insurance. Whether you're shipping within Europe or internationally, GLS can help you get your packages delivered on time and in good condition.
+        </div>
+    </div>
+</div>
+
+@endsection
